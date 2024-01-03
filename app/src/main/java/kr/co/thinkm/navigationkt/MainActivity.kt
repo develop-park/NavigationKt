28064e6fc34c55dba39ce374ac9b570ad0309826
develop-park <hspark0909@gmail.com>
2024-01-03 17:31:58 +0900

    공부중2

--- app/src/main/java/kr/co/thinkm/navigationkt/MainActivity.kt
+++ app/src/main/java/kr/co/thinkm/navigationkt/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         naviView.setNavigationItemSelectedListener(this)
 
 
-        // 공부중1.....
+        // 공부중2.....
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
